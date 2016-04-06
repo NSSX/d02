90b56e6112e8e3c8daa877fa6e097d38874d671e
Anthony VELLA <avella@e3r11p11.42.fr>
2016-04-06 15:24:39 +0200

    l

--- ex02/loupe.php
+++ ex02/loupe.php
@@ -1,4 +1,47 @@
 #!/usr/bin/php
 <?php
 
+if($argc > 1)
+{
+$myfile = fopen("$argv[1]", "r+");
+$chaine = "";
+while($ligne = fgets($myfile))
+{
+$chaine= $chaine.$ligne;
+}
+$nu = strlen("$chaine");
+$i = 0;
+/*while($i < $nu)
+{
+	
+	if($chaine[$i] == '<' && $chaine[$i] == '<')
+
+
+
+
+	$i++;
+}*/
+
+$cont = "";
+$new = preg_replace_callback("#<a(.*?)>(.*?)</a>#",     function ($matches) {
+           	
+           	 return (preg_replace_callback("#>(.*?)<#",     function ($a) {
+           	 	return strtoupper($a[0]);
+           	 }, $matches[0]));
+           	
+        },
+        $chaine
+    );
+$new = preg_replace_callback("#title=\"(.*?)\"#",     function ($matches) {
+ 	 			return (preg_replace_callback("#\"(.*?)\"#",     function ($a) {
+           	 		return strtoupper($a[0]);
+           	 }, $matches[0]));
+ 	 			}, 
+           	 $new
+           	 );
+
+echo "$new";
+//echo"$chaine";
+fclose($myfile);
+}
 ?>
